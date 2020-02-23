0b16d630a59e518eda043871b905ddb96e517103
manuel-pancorbo <manuel.pancorbo@scmspain.com>
2020-02-23 23:11:43 +0100

    Added the real implementation of password encrypter service in configuration

--- src/authentication/main/kotlin/com/readify/authentication/infrastructure/configuration/PlainUserCredentialsCreatedSubscriberConfiguration.kt
+++ src/authentication/main/kotlin/com/readify/authentication/infrastructure/configuration/PlainUserCredentialsCreatedSubscriberConfiguration.kt
@@ -1,25 +1,24 @@
 package com.readify.authentication.infrastructure.configuration
 
 import com.readify.authentication.application.service.usercredentials.CreateUserCredentialsService
-import com.readify.authentication.domain.usercredentials.EncryptedPassword
 import com.readify.authentication.domain.usercredentials.PasswordEncrypterService
 import com.readify.authentication.domain.usercredentials.UserCredentials
 import com.readify.authentication.domain.usercredentials.UserCredentialsRepository
+import com.readify.authentication.infrastructure.domain.usercredentials.BcryptPasswordEncrypterService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class CreateUserCredentialsConfiguration {
     @Bean
-    fun service() = CreateUserCredentialsService(object : PasswordEncrypterService {
-        override fun encrypt(plainPassword: String): EncryptedPassword {
-            return EncryptedPassword("test")
-        }
+    fun service(passwordEncrypterService: PasswordEncrypterService) =
+        CreateUserCredentialsService(passwordEncrypterService, object : UserCredentialsRepository {
+            override fun save(userCredentials: UserCredentials) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
 
-    }, object : UserCredentialsRepository {
-        override fun save(userCredentials: UserCredentials) {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
+        })
 
-    })
+    @Bean
+    fun passwordEncrypterService() = BcryptPasswordEncrypterService()
 }
