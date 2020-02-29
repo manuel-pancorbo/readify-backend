4714a4b31f7ac52fc2eafc06b49c5ad7fff36282
manuel-pancorbo <manuel.pancorbo@scmspain.com>
2020-02-29 16:56:36 +0100

    Extracted delta expiration to constant

--- src/authentication/main/kotlin/com/readify/authentication/infrastructure/domain/accesstoken/JwtAccessTokenGenerator.kt
+++ src/authentication/main/kotlin/com/readify/authentication/infrastructure/domain/accesstoken/JwtAccessTokenGenerator.kt
@@ -9,12 +9,13 @@ import java.util.Date
 
 private const val USERNAME_CLAIM = "username"
 private const val EMAIL_CLAIM = "email"
+private const val EXPIRATION_DELTA_IN_HOURS = 24L
 
 class JwtAccessTokenGenerator(private val signingSecretKey: String, private val clock: Clock) : AccessTokenGenerator {
     override fun generate(userCredentials: UserCredentials) =
         JWT.create()
             .withIssuer(userCredentials.userId.value)
-            .withExpiresAt(Date.from(clock.now().plusHours(24).toInstant()))
+            .withExpiresAt(Date.from(clock.now().plusHours(EXPIRATION_DELTA_IN_HOURS).toInstant()))
             .withClaim(USERNAME_CLAIM, userCredentials.username.value)
             .withClaim(EMAIL_CLAIM, userCredentials.email.value)
             .sign(Algorithm.HMAC256(signingSecretKey))
