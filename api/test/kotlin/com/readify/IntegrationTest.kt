b27404cd78a3abb2ef736dfc34d1cfe7299b6378
manuel-pancorbo <manuel.pancorbo@scmspain.com>
2020-03-06 19:47:52 +0100

    Added tests containers for running integration tests

--- api/test/kotlin/com/readify/IntegrationTest.kt
+++ api/test/kotlin/com/readify/IntegrationTest.kt
@@ -4,7 +4,21 @@ import com.readify.api.Application
 import io.mockk.junit5.MockKExtension
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.testcontainers.containers.DockerComposeContainer
+import org.testcontainers.containers.wait.strategy.Wait
+import org.testcontainers.junit.jupiter.Container
+import java.io.File
 
 @ExtendWith(MockKExtension::class)
 @SpringBootTest(classes = [Application::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-abstract class IntegrationTest
+abstract class IntegrationTest {
+    companion object {
+        @Container
+        val docker: Unit = KotlinDockerComposeContainer(File("docker-compose.yml"))
+            .withLocalCompose(true)
+            .withExposedService("postgresql", 5432, Wait.defaultWaitStrategy())
+            .start()
+    }
+}
+
+class KotlinDockerComposeContainer(vararg files: File) : DockerComposeContainer<KotlinDockerComposeContainer>(*files)
