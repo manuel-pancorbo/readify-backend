a6d2c248bde9e3474e11831d5057b38ffc3688ba
manuel-pancorbo <manuel.pancorbo@scmspain.com>
2020-04-09 20:41:49 +0200

    Added boilerplate for creating a new bounded context: reader library

---
+++ src/readerlibrary/main/kotlin/resources/reader-library-init.sql
@@ -0,0 +1,32 @@
+create schema if not exists readerlibrary;
+CREATE TABLE readerlibrary.book
+(
+    id                   VARCHAR(36)  NOT NULL,
+    authorId             VARCHAR(36)  NOT NULL,
+    status               VARCHAR(15),
+    title                VARCHAR(150) NOT NULL,
+    summary              TEXT,
+    cover                VARCHAR(200),
+    tags                 VARCHAR(20)[],
+    priceamount          NUMERIC(6, 2),
+    pricecurrency        VARCHAR(4),
+    finishedAt           timestamp with time zone,
+    completionpercentage NUMERIC(3, 0),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE readerlibrary.chapter
+(
+    id            VARCHAR(36)  NOT NULL,
+    authorId      VARCHAR(36)  NOT NULL,
+    bookId        VARCHAR(36)  NOT NULL,
+    title         VARCHAR(150) NOT NULL,
+    content       TEXT,
+    modifiedAt    timestamp,
+    publishedAt   timestamp,
+    priceamount   NUMERIC(6, 2),
+    pricecurrency VARCHAR(4),
+    chapterOrder  NUMERIC(2, 0),
+    excerpt       TEXT,
+    PRIMARY KEY (id)
+);
