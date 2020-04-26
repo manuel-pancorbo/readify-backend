e78839052006eed9b7ff04d56d6c601f45d1491f
manuel-pancorbo <manuel.pancorbo@adevinta.com>
2020-04-26 16:18:48 +0200

    fixed configuration

--- src/bookpublishing/main/kotlin/com/readify/bookpublishing/infrastructure/configuration/GetBookServiceConfiguration.kt
+++ src/bookpublishing/main/kotlin/com/readify/bookpublishing/infrastructure/configuration/GetBookServiceConfiguration.kt
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class GetBookServiceConfiguration {
+class GetAuthorBookServiceConfiguration {
     @Bean
-    fun getBookService(bookRepository: BookRepository) = GetAuthorBookService(bookRepository)
+    fun getAuthorBookService(bookRepository: BookRepository) = GetAuthorBookService(bookRepository)
 }
